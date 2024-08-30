75d8686c909c0d71ac953265ded50667e4cfd7a5
seasunflower <phamdinhhaiquynh@gmail.com>
2024-08-30 16:53:05 +0200

    add API keys

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
+from dotenv import load_dotenv
 
 
 def main():
+    load_dotenv()
     st.set_page_config(page_title="Chat with multiple PDFs",
                        page_icon=":books:")
     st.header("Chat with multiple PDFs :books:")
