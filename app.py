e71946d83300f851f1c737800219bccfadb5240f
seasunflower <phamdinhhaiquynh@gmail.com>
2024-09-08 16:51:51 +0200

    Display chat history

--- app.py
+++ app.py
@@ -42,6 +42,16 @@ def get_conversation_chain(vectorstore):
     )
     return conversation_chain
 
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat with multiple PDFs",
@@ -55,10 +65,9 @@ def main():
     
     st.header("Chat with multiple PDFs :books:")
     st.text_input("Ask a question about your documents:")
+    if user_question:
+        handle_userinput(user_question)
     
-    st.write(user_template.replace("{{MSG}}", "Hello Bot"), unsafe_allow_html = True)
-    st.write(bot_template.replace("{{MSG}}", "Hello Human"), unsafe_allow_html = True)
-
     with st.sidebar:
         st.subheader("Your documents")
         pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
